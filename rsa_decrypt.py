1a70b022e5a157d90c4c9f46c81456448977a2c6
divydividivu <64409788+divydividivu@users.noreply.github.com>
2021-02-07 19:24:17 +0530

    Update rsa_decrypt.py

--- rsa_decrypt.py
+++ rsa_decrypt.py
@@ -3,13 +3,13 @@
 from Crypto.Util.number import *
 from factordb.factordb import FactorDB
 
-
+print("Let's try to decrypt RSA encrypted cipher")
 n = int(input("ENTER n : "))
 e = int(input("ENTER e : "))
 c = int(input("ENTER c : "))
 
 
-f=FactorDB(n)
+f = FactorDB(n) 
 f.get_factor_list()
 f.connect()
 result=f.get_factor_list()
@@ -21,11 +21,11 @@ phi = (p-1)*(q-1)
 
 d = inverse(e,phi)
 
-m=pow(c,d,n)
+m = pow(c,d,n)
 
 print("The message is :")
 print(m)
-print("-"*69)
+print("-"*70)
 print("The message after long_to_bytes is :")
 print(long_to_bytes(m))
 
